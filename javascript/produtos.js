18b28471cd840f60bf70c32ed47561f09213b99f
Pedro Souza <opedrosouza@hotmail.com>
2021-01-30 16:47:18 -0300

    first commit

---
+++ javascript/produtos.js
@@ -0,0 +1,30 @@
+let produtos = [
+  {
+    id: 1,
+    image: 'https://picsum.photos/200',
+    title: 'Prato 01',
+    description: 'Prato 01 desc',
+    value: 10.00
+  },
+  {
+    id: 2,
+    image: 'https://picsum.photos/200',
+    title: 'Prato 02',
+    description: 'Prato 02 desc',
+    value: 20.00
+  },
+  {
+    id: 3,
+    image: 'https://picsum.photos/200',
+    title: 'Prato 03',
+    description: 'Prato 03 desc',
+    value: 30.00
+  },
+  {
+    id: 4,
+    image: 'https://picsum.photos/200',
+    title: 'Prato 04',
+    description: 'Prato 04 desc',
+    value: 40.00
+  },
+]
