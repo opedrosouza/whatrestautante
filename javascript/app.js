7189d33de34f87aa732cf3757bac71fc5bf31946
Pedro Souza <opedrosouza@hotmail.com>
2021-01-30 19:13:53 -0300

    Added basic search feature

--- javascript/app.js
+++ javascript/app.js
@@ -44,5 +44,38 @@ const checkout = (phoneNumber) => {
   })
 }
 
+const search = (products, searchTerm) => products.filter( product => product.title.includes(`${searchTerm}`) || product.description.includes(`${searchTerm}`)) 
+
+const loadSearch = (form, productsDivId) => {
+  const input = form.querySelector('#inputSearch')
+  const productsDiv = document.querySelector(productsDivId)
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (input.value != '' && input.value != null) {
+      productsDiv.querySelectorAll('.prato').forEach(prato => {
+        prato.remove()
+      })
+
+      let results = search(produtos, input.value)
+      results.forEach(product => {
+        const html = `
+          <article class="prato">
+            <img src="${product.image}" alt="${product.title}">
+            <h4>${product.title}</h4>
+            <h4>R$ ${product.value}</h4>
+            <p>${product.description}</p>
+            <button type="button" onclick="modalTrigger(${product.id})">Quero este prato</button>
+          </article>
+        `
+        productsDiv.insertAdjacentHTML('beforeend', html)
+      })
+    }
+  })
+}
+
 loadProducts(produtos, '#pratos-div')
 checkout('5596984020861')
+
+const form = document.querySelector('#formSearch')
+loadSearch(form, '#pratos-div', '#buscas-div')
